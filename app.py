3e93c998d5a4c32d3eec13abf58c43d063424f80
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-03 21:27:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '@JishuDeveloper'
 
-
 if __name__ == "__main__":
-    app.run()
+    # Render / Koyeb வழங்கும் PORT-ஐ எடுத்து இயங்கும் வகையில் அமைக்கப்பட்டுள்ளது
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
