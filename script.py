ea67bda4d6af80c75ddd46001dce450ee2cec95b
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-04 01:11:54 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,9 +3,9 @@ class script(object):
     
 <b>Here Is The Help For My Commands.</b>"""
 
-    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
 
-<b>⦿ /set_caption - Use This Command To Set Your Caption</b>
+<b>⦿ /set_caption - Use This Command To Set Your Caption</b>
 <b>⦿ /see_caption - Use This Command To See Your Caption</b>
 <b>⦿ /del_caption - Use This Command To Delete Your Caption</b>"""
    
@@ -32,19 +32,18 @@ If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upt
 
 <b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
 
-<b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
+<b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b>"""
 
     ADMIN_TXT = """<b><u>🦋 ADMIN ALL COMMANDS HERE</u></b>
 
-<b>⦿ /users - Use This Command To See Total Users</b>
-<b>⦿ /allids - Use This Command To See All Users IDs</b>
+<b>⦿ /users - Use This Command To See Total Users</b>
+<b>⦿ /allids - Use This Command To See All Users IDs</b>
 <b>⦿ /broadcast - Use This Command To Send A Message To Users</b>
 <b>⦿ /warn - Use This Command To Send A Message To A User</b>
 <b>⦿ /resetpower - Use This Command To Reset User Power</b>
 <b>⦿ /ceasepower - Use This Command To Cease User Power</b>
 <b>⦿ /addpremium - Use This Command To Add Premium To Users</b>
 <b>⦿ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
-
 
     METADATA_TXT = """<b><u>🖼️  HOW TO SET CUSTOM METADATA</u></b>
 
@@ -54,10 +53,6 @@ For Example :-
 
 💬 For Any Help Contact @MadflixOfficials
 """
-
-
-
-
 
 
 # Jishu Developer 
