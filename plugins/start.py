1fe6e445ac400391fee15f911aaa9a5eec3fda80
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-03 21:23:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,156 +1,231 @@
-from datetime import date as date_
 import os, re, datetime, random, asyncio, time, humanize
-from script import *
+from datetime import date as date_
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram import Client, filters, enums
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from helper.progress import humanbytes
-from helper.database import botdata, find_one, total_user
-from helper.database import insert, find_one, used_limit, usertype, uploadlimit, addpredata, total_rename, total_size
+from helper.database import (
+    botdata, find_one, total_user, insert, 
+    used_limit, uploadlimit, addpredata, total_rename, total_size
+)
 from pyrogram.file_id import FileId
 from helper.database import daily as daily_
-from helper.date import check_expi
 from config import *
 
 token = BOT_TOKEN
 botid = token.split(':')[0]
 
+# 4GB in Bytes
+FOUR_GB = 4294967296
 
-
+# Helper function for Loading Animation
+async def play_loading_animation(message_or_query):
+    if isinstance(message_or_query, CallbackQuery):
+        msg = await message_or_query.message.reply_text("<code>Loading.</code>")
+    else:
+        msg = await message_or_query.reply_text("<code>Loading.</code>")
+        
+    await asyncio.sleep(0.5)
+    await msg.edit_text("<code>Loading..</code>")
+    await asyncio.sleep(0.5)
+    await msg.edit_text("<code>Loading...</code>")
+    await asyncio.sleep(0.5)
+    await msg.delete()
 
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     user_id = message.chat.id
-    old = insert(int(user_id))
+    insert(int(user_id))
     
-    try:
-        id = message.text.split(' ')[1]
-    except IndexError:
-        id = None
+    # 1. First run the Loading Animation ALWAYS
+    await play_loading_animation(message)
+    
+    # 2. Check Force Subscription after loading
+    if FORCE_SUBS:
+        try:
+            await client.get_chat_member(FORCE_SUBS, user_id)
+        except UserNotParticipant:
+            await message.reply_text(
+                "<b>Hello Dear \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>",
+                reply_to_message_id=message.id,
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("🔺 Update Channel 🔺", url=f"https://t.me/{FORCE_SUBS}")],
+                    [InlineKeyboardButton("🔄 Try Again", callback_data="try_again")]
+                ])
+            )
+            return
 
-    loading_sticker_message = await message.reply_sticker("CAACAgIAAxkBAALmzGXSSt3ppnOsSl_spnAP8wHC26jpAAJEGQACCOHZSVKp6_XqghKoHgQ")
-    await asyncio.sleep(2)
-    await loading_sticker_message.delete()
-    
-    text = f"""Hello {message.from_user.mention} \n\n➻ This Is An Advanced And Yet Powerful Rename Bot.\n\n➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.\n\n➻ You Can Also Convert Video To File Aɴᴅ File To Video.\n\n➻ This Bot Also Supports Custom Thumbnail And Custom Caption.\n\n<b>Bot Is Made By @Madflix_Bots</b>"""
-    
+    # 3. Show Start Text & Buttons if Force Sub passed
+    text = START_TXT.format(mention=message.from_user.mention)
     button = InlineKeyboardMarkup([
-        [InlineKeyboardButton("📢 Updates", url="https://t.me/Madflix_Bots"),
-        InlineKeyboardButton("💬 Support", url="https://t.me/MadflixBots_Support")],
-        [InlineKeyboardButton("🛠️ Help", callback_data='help'),
-        InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
-        [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://t.me/MadflixOfficials")]
-        ])
+        [
+            InlineKeyboardButton("❤️‍🩹 About", callback_data='about'),
+            InlineKeyboardButton("🛠️ Help", callback_data='help')
+        ],
+        [
+            InlineKeyboardButton("✖️ Close", callback_data='close')
+        ]
+    ])
     
     await message.reply_photo(
         photo=START_PIC,
         caption=text,
         reply_markup=button,
         quote=True
-        )
-    return    
+    )
 
+
+@Client.on_callback_query(filters.regex("^(about|help|back|close|try_again)$"))
+async def callback_handler(client, query: CallbackQuery):
+    data = query.data
+    user_id = query.from_user.id
+    
+    if data == "try_again":
+        await query.message.delete()
+        # Play loading animation again on retry
+        await play_loading_animation(query)
+        
+        if FORCE_SUBS:
+            try:
+                await client.get_chat_member(FORCE_SUBS, user_id)
+            except UserNotParticipant:
+                await query.message.reply_text(
+                    "<b>Hello Dear \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>",
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("🔺 Update Channel 🔺", url=f"https://t.me/{FORCE_SUBS}")],
+                        [InlineKeyboardButton("🔄 Try Again", callback_data="try_again")]
+                    ])
+                )
+                return
+
+        text = START_TXT.format(mention=query.from_user.mention)
+        button = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("❤️‍🩹 About", callback_data='about'),
+                InlineKeyboardButton("🛠️ Help", callback_data='help')
+            ],
+            [
+                InlineKeyboardButton("✖️ Close", callback_data='close')
+            ]
+        ])
+        await client.send_photo(
+            chat_id=query.message.chat.id,
+            photo=START_PIC,
+            caption=text,
+            reply_markup=button
+        )
+
+    elif data == "about":
+        text = ABOUT_TXT
+        button = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("🔙 Back", callback_data="back"),
+                InlineKeyboardButton("✖️ Close", callback_data="close")
+            ]
+        ])
+        await query.message.edit_text(text=text, reply_markup=button, disable_web_page_preview=True)
+
+    elif data == "help":
+        text = HELP_TXT
+        button = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("🔙 Back", callback_data="back"),
+                InlineKeyboardButton("✖️ Close", callback_data="close")
+            ]
+        ])
+        await query.message.edit_text(text=text, reply_markup=button, disable_web_page_preview=True)
+
+    elif data == "back":
+        text = START_TXT.format(mention=query.from_user.mention)
+        button = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("❤️‍🩹 About", callback_data='about'),
+                InlineKeyboardButton("🛠️ Help", callback_data='help')
+            ],
+            [
+                InlineKeyboardButton("✖️ Close", callback_data='close')
+            ]
+        ])
+        await query.message.edit_text(text=text, reply_markup=button)
+
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except Exception:
+            pass
 
 
 @Client.on_message((filters.private & (filters.document | filters.audio | filters.video)) | filters.channel & (filters.document | filters.audio | filters.video))
 async def send_doc(client, message):
-    user_id = message.chat.id
-    old = insert(int(user_id))
+    chat_id = message.chat.id
+    from_user = message.from_user
+    user_id = from_user.id if from_user else chat_id
+
+    insert(int(user_id))
         
-    user_id = message.from_user.id    
-    if FORCE_SUBS:
+    if FORCE_SUBS and from_user:
         try:
             await client.get_chat_member(FORCE_SUBS, user_id)
         except UserNotParticipant:
-            _newus = find_one(message.from_user.id)
-            user = _newus["usertype"]
-            await message.reply_text("<b>Hello Dear \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>",
-                                     reply_to_message_id=message.id,
-                                     reply_markup=InlineKeyboardMarkup([
-                                         [InlineKeyboardButton("🔺 Update Channel 🔺", url=f"https://t.me/{FORCE_SUBS}")]
-                                         ]))
-            await client.send_message(LOG_CHANNEL, f"<b><u>New User Started The Bot</u></b> \n\n<b>User ID :</b> <code>{user_id}</code> \n<b>First Name :</b> {message.from_user.first_name} \n<b>Last Name :</b> {message.from_user.last_name} \n<b>User Name :</b> @{message.from_user.username} \n<b>User Mention :</b> {message.from_user.mention} \n<b>User Link :</b> <a href='tg://openmessage?user_id={user_id}'>Click Here</a> \n<b>User Plan :</b> {user}")
+            await message.reply_text(
+                "<b>Hello Dear \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>",
+                reply_to_message_id=message.id,
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("🔺 Update Channel 🔺", url=f"https://t.me/{FORCE_SUBS}")],
+                    [InlineKeyboardButton("🔄 Try Again", callback_data="try_again")]
+                ])
+            )
             return
-		
+
     botdata(int(botid))
     bot_data = find_one(int(botid))
     prrename = bot_data['total_rename']
     prsize = bot_data['total_size']
+    
     user_deta = find_one(user_id)
     used_date = user_deta["date"]
-    buy_date = user_deta["prexdate"]
-    daily = user_deta["daily"]
-    user_type = user_deta["usertype"]
 
     c_time = time.time()
-
-    if user_type == "Free":
-        LIMIT = 120
-    else:
-        LIMIT = 10
+    LIMIT = 10  # Standard flood limit
+    
     then = used_date + LIMIT
     left = round(then - c_time)
-    conversion = datetime.timedelta(seconds=left)
-    ltime = str(conversion)
     if left > 0:
-        await message.reply_text(f"<b>Sorry Dude I Am Not Only For You \n\nFlood Control Is Active So Please Wait For {ltime} </b>", reply_to_message_id=message.id)
-    else:
-        # Forward a single message
-        media = await client.get_messages(message.chat.id, message.id)
-        file = media.document or media.video or media.audio
-        dcid = FileId.decode(file.file_id).dc_id
-        filename = file.file_name
-        file_id = file.file_id
-        value = 2147483648
-        used_ = find_one(message.from_user.id)
-        used = used_["used_limit"]
-        limit = used_["uploadlimit"]
-        expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
-        if expi != 0:
-            today = date_.today()
-            pattern = '%Y-%m-%d'
-            epcho = int(time.mktime(time.strptime(str(today), pattern)))
-            daily_(message.from_user.id, epcho)
-            used_limit(message.from_user.id, 0)
-        remain = limit - used
-        if remain < int(file.file_size):
-            await message.reply_text(f"100% Of Daily {humanbytes(limit)} Data Quota Exhausted.\n\n<b>File Size Detected :</b> {humanbytes(file.file_size)}\n<b>Used Daily Limit :</b> {humanbytes(used)}\n\nYou Have Only <b>{humanbytes(remain)}</b> Left On Your Account.\n\nIf U Want To Rename Large File Upgrade Your Plan", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💳 Upgrade", callback_data="upgrade")]]))
-            return
-        if value < file.file_size:
-            
-            if STRING_SESSION:
-                if buy_date == None:
-                    await message.reply_text(f"You Can't Upload More Than 2GB File.\n\nYour Plan Doesn't Allow To Upload Files That Are Larger Than 2GB.\n\nUpgrade Your Plan To Rename Files Larger Than 2GB.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💳 Upgrade", callback_data="upgrade")]]))
-                    return
-                pre_check = check_expi(buy_date)
-                if pre_check == True:
-                    await message.reply_text(f"""__What Do You Want Me To Do With This File ?__\n\n**File Name :** `{filename}`\n**File Size :** {humanize.naturalsize(file.file_size)}\n**DC ID :** {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 Rename", callback_data="rename"), InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]]))
-                    total_rename(int(botid), prrename)
-                    total_size(int(botid), prsize, file.file_size)
-                else:
-                    uploadlimit(message.from_user.id, 2147483648)
-                    usertype(message.from_user.id, "Free")
+        ltime = str(datetime.timedelta(seconds=left))
+        await message.reply_text(
+            f"<b>Flood Control Is Active. Please Wait For {ltime} </b>", 
+            reply_to_message_id=message.id
+        )
+        return
 
-                    await message.reply_text(f'Your Plan Expired On {buy_date}', quote=True)
-                    return
-            else:
-                await message.reply_text("You Can't Upload More Than 2GB File.\n\nYour Plan Doesn't Allow To Upload Files That Are Larger Than 2GB.\n\nUpgrade Your Plan To Rename Files Larger Than 2GB.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💳 Upgrade", callback_data="upgrade")]]))
-                return
-        else:
-            if buy_date:
-                pre_check = check_expi(buy_date)
-                if pre_check == False:
-                    uploadlimit(message.from_user.id, 2147483648)
-                    usertype(message.from_user.id, "Free")
-            
-            filesize = humanize.naturalsize(file.file_size)
-            fileid = file.file_id
-            total_rename(int(botid), prrename)
-            total_size(int(botid), prsize, file.file_size)
-            await message.reply_text(f"""__What Do You Want Me To Do With This File ?__\n\n**File Name :** `{filename}`\n**File Size :** {filesize}\n**DC ID :** {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("📝 Rename", callback_data="rename"),
-                  InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]]))
-              
-              
-              
+    media = await client.get_messages(chat_id, message.id)
+    file = media.document or media.video or media.audio
+    dcid = FileId.decode(file.file_id).dc_id
+    filename = file.file_name
+
+    # 4GB Check
+    if file.file_size > FOUR_GB:
+        await message.reply_text("You Can't Upload Files Larger Than 4GB.")
+        return
+
+    if file.file_size > 2147483648 and not STRING_SESSION:
+        await message.reply_text("Cannot Process Files Larger Than 2GB Without Userbot String Session Configured.")
+        return
+
+    filesize = humanize.naturalsize(file.file_size)
+    total_rename(int(botid), prrename)
+    total_size(int(botid), prsize, file.file_size)
+
+    await message.reply_text(
+        f"__What Do You Want Me To Do With This File ?__\n\n"
+        f"**File Name :** `{filename}`\n"
+        f"**File Size :** {filesize}\n"
+        f"**DC ID :** {dcid}",
+        reply_to_message_id=message.id,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("📝 Rename", callback_data="rename"),
+             InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
+        ])
+    )
