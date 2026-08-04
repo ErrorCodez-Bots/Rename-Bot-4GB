fea853c1ee8fe80a7bd69a93e6679bd91ec762eb
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-04 17:19:25 +0530

    Rename environment variables for database config

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 # Mongo DB Database Config
-DB_URL = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 # Single Start Picture (Fallback)
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
