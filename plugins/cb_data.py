af5754bdf125b51c66c885c18de2474be054ca2b
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-04 15:36:26 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -1,374 +1,102 @@
-from helper.progress import progress_for_pyrogram
 from pyrogram import Client, filters
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from helper.database import *
-import os, random, time, asyncio, humanize
-from PIL import Image
-from datetime import timedelta
-from helper.ffmpeg import take_screen_shot, fix_thumb, add_metadata
-from helper.progress import humanbytes
-from helper.set import escape_invalid_curly_brackets
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from config import *
 
-# Main bot-ல் இருந்து userbot client-ஐக் கொண்டு வருதல்
-from bot import userbot as app
+# ==========================================
+#          CALLBACK QUERY HANDLER
+# ==========================================
 
+@Client.on_callback_query(filters.regex('^(about|help|home|back|donate|more|close|cancel|try_again)$'))
+async def cb_handler(bot, query: CallbackQuery):
+    data = query.data
+    user_id = query.from_user.id
+    f_sub = FORCE_SUBS.replace("@", "") if FORCE_SUBS else None
 
-@Client.on_callback_query(filters.regex('cancel'))
-async def cancel(bot, update):
-    try:
-        await update.message.delete()
-        await update.message.reply_to_message.delete()
-        await update.message.continue_propagation()
-    except:
-        await update.message.delete()
-        await update.message.continue_propagation()
-        return
+    # 1. HOME / BACK TO MAIN START MENU
+    if data in ["home", "back"]:
+        keyboard = InlineKeyboardMarkup([  
+            [InlineKeyboardButton("• ᴄʟɪᴄᴋ ꜰᴏʀ ᴍᴏʀᴇ •", callback_data="more")],
+            [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
+             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url=UPDATE_CHANNEL)],
+            [InlineKeyboardButton("ᴅᴏɴᴀᴛᴇ", callback_data='donate')]
+        ])
+        await query.message.edit_text(
+            text=START_TXT.format(mention=query.from_user.mention), 
+            reply_markup=keyboard
+        )
 
+    # 2. CLICK FOR MORE MENU
+    elif data == "more":
+        keyboard = InlineKeyboardMarkup([ 
+            [InlineKeyboardButton("« ʙᴀᴄᴋ", callback_data='home'),
+             InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ", callback_data='close')]
+        ])
+        await query.message.edit_text(
+            text=MORE_TXT, 
+            reply_markup=keyboard, 
+            disable_web_page_preview=True
+        )
 
-@Client.on_callback_query(filters.regex('rename'))
-async def rename(bot, update):
-    date_fa = str(update.message.date)
-    pattern = '%Y-%m-%d %H:%M:%S'
-    date = int(time.mktime(time.strptime(date_fa, pattern)))
-    chat_id = update.message.chat.id
-    id = update.message.reply_to_message_id
-    await update.message.delete()
-    await update.message.reply_text(f"__Please Enter The New Filename...__\n\n**Note :** Extension Not Required", reply_to_message_id=id,
-                                    reply_markup=ForceReply(True))
-    dateupdate(chat_id, date)
+    # 3. HELP MENU
+    elif data == "help":
+        keyboard = InlineKeyboardMarkup([ 
+            [InlineKeyboardButton("« ʙᴀᴄᴋ", callback_data='home'),
+             InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ", callback_data='close')]
+        ])
+        await query.message.edit_text(
+            text=HELP_TXT, 
+            reply_markup=keyboard, 
+            disable_web_page_preview=True
+        )
 
+    # 4. DONATE MENU
+    elif data == "donate":
+        keyboard = InlineKeyboardMarkup([  
+            [InlineKeyboardButton("« ʙᴀᴄᴋ", callback_data='home'),
+             InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ", callback_data="close")]
+        ])
+        await query.message.edit_text(
+            text=DONATE_TXT, 
+            reply_markup=keyboard, 
+            disable_web_page_preview=True
+        )
 
-@Client.on_callback_query(filters.regex("doc"))
-async def doc(bot, update):
-
-    if not os.path.isdir("Metadata"):
-        os.mkdir("Metadata")
-
-    new_name = update.message.text
-    used_ = find_one(update.from_user.id)
-    used = used_["used_limit"]
-    date = used_["date"]
-    new_filename = new_name.split(":-")[1]
-    file_path = f"downloads/{new_filename}"
-    message = update.message.reply_to_message
-    file = message.document or message.video or message.audio
-    hinata = message
-    ms = await update.message.edit("🚀 Try To Download...  ⚡")
-    used_limit(update.from_user.id, file.file_size)
-    c_time = time.time()
-    total_used = used + int(file.file_size)
-    used_limit(update.from_user.id, total_used)
-    try:
-        path = await bot.download_media(message=file, progress=progress_for_pyrogram, progress_args=("🚀 Try To Downloading...  ⚡",  ms, c_time))
-
-    except Exception as e:
-        neg_used = used - int(file.file_size)
-        used_limit(update.from_user.id, neg_used)
-        await ms.edit(str(e))
-        return
-    
-    _bool_metadata = find(int(message.chat.id))[2] 
-    
-    if _bool_metadata:
-        metadata = find(int(message.chat.id))[3]
-        metadata_path = f"Metadata/{new_filename}"
-        await add_metadata(path, metadata_path, metadata, ms)
-    else:
-        await ms.edit("🚀 Mode Changing...  ⚡")
-
-    splitpath = path.split("/downloads/")
-    dow_file_name = splitpath[1]
-    old_file_name = f"downloads/{dow_file_name}"
-    os.rename(old_file_name, file_path)
-    user_id = int(update.message.chat.id)
-    data = find(user_id)
-    try:
-        c_caption = data[1]
-    except:
-        pass
-    thumb = data[0]
-    if c_caption:
-        doc_list = ["filename", "filesize"]
-        new_tex = escape_invalid_curly_brackets(c_caption, doc_list)
-        caption = new_tex.format(
-            filename=new_filename, filesize=humanbytes(file.file_size))
-    else:
-        caption = f"**{new_filename}**"
-    if thumb:
-        ph_path = await bot.download_media(thumb)
-        Image.open(ph_path).convert("RGB").save(ph_path)
-        img = Image.open(ph_path)
-        img.resize((320, 320))
-        img.save(ph_path, "JPEG")
-        c_time = time.time()
-
-    else:
-        ph_path = None
-
-    value = 2090000000
-    if value < file.file_size:
-        await ms.edit("🚀 Try To Upload...  ⚡")
+    # 5. CLOSE MENU
+    elif data in ["close", "cancel"]:
+        await query.message.delete()
         try:
-            client_to_use = app if app else bot
-            filw = await client_to_use.send_document(int(LOG_CHANNEL), document=metadata_path if _bool_metadata else file_path, thumb=ph_path, caption=caption, progress=progress_for_pyrogram, progress_args=("🚀 Try To Uploading...  ⚡",  ms, c_time))
-            from_chat = filw.chat.id
-            mg_id = filw.id
-            time.sleep(2)
-            await bot.copy_message(update.from_user.id, from_chat, mg_id)
-            await ms.delete()
-            
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            if ph_path and os.path.exists(ph_path):
-                os.remove(ph_path)
-            
-        except Exception as e:
-            neg_used = used - int(file.file_size)
-            used_limit(update.from_user.id, neg_used)
-            await ms.edit(str(e))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            if ph_path and os.path.exists(ph_path):
-                os.remove(ph_path)
-            return
-    else:
-        await ms.edit("🚀 Try To Upload...  ⚡")
-        c_time = time.time()
-        try:
-            await bot.send_document(update.from_user.id, document=metadata_path if _bool_metadata else file_path, thumb=ph_path, caption=caption, progress=progress_for_pyrogram, progress_args=("🚀 Try To Uploading...  ⚡",  ms, c_time))
-            await ms.delete()
-            
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            
-        except Exception as e:
-            neg_used = used - int(file.file_size)
-            used_limit(update.from_user.id, neg_used)
-            await ms.edit(str(e))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            return
+            await query.message.reply_to_message.delete()
+        except Exception:
+            pass
 
+    # 6. TRY AGAIN FOR FORCE SUB
+    elif data == "try_again":
+        await query.message.delete()
+        if f_sub:
+            try:
+                await bot.get_chat_member(f_sub, user_id)
+            except Exception:
+                text = (
+                    "<b><blockquote>ʜᴇʟʟᴏ ᴅᴇᴀʀ 👋\n\n"
+                    "ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜꜱᴇ ᴍᴇ.\n"
+                    "ᴋɪɴᴅʟʏ ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ.</blockquote></b>"
+                )
+                await query.message.reply_text(
+                    text=text,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("🔺 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🔺", url=f"https://t.me/{f_sub}")],
+                        [InlineKeyboardButton("🔄 ᴛʀʏ ᴀɢᴀɪɴ", callback_data="try_again")]
+                    ])
+                )
+                return
 
-@Client.on_callback_query(filters.regex("vid"))
-async def vid(bot, update):
-
-    if not os.path.isdir("Metadata"):
-        os.mkdir("Metadata")
-
-    new_name = update.message.text
-    used_ = find_one(update.from_user.id)
-    used = used_["used_limit"]
-    date = used_["date"]
-    name = new_name.split(":-")
-    new_filename = name[1]
-    file_path = f"downloads/{new_filename}"
-    message = update.message.reply_to_message
-    file = message.document or message.video or message.audio
-    hinata = message
-    ms = await update.message.edit("🚀 Try To Download...  ⚡")
-    used_limit(update.from_user.id, file.file_size)
-    c_time = time.time()
-    total_used = used + int(file.file_size)
-    used_limit(update.from_user.id, total_used)
-    try:
-        path = await bot.download_media(message=file, progress=progress_for_pyrogram, progress_args=("🚀 Try To Downloading...  ⚡",  ms, c_time))
-
-    except Exception as e:
-        neg_used = used - int(file.file_size)
-        used_limit(update.from_user.id, neg_used)
-        await ms.edit(str(e))
-        return
-    
-    _bool_metadata = find(int(message.chat.id))[2] 
-    
-    if _bool_metadata:
-        metadata = find(int(message.chat.id))[3]
-        metadata_path = f"Metadata/{new_filename}"
-        await add_metadata(path, metadata_path, metadata, ms)
-    else:
-        await ms.edit("🚀 Mode Changing...  ⚡") 
-
-    splitpath = path.split("/downloads/")
-    dow_file_name = splitpath[1]
-    old_file_name = f"downloads/{dow_file_name}"
-    os.rename(old_file_name, file_path)
-    user_id = int(update.message.chat.id)
-    data = find(user_id)
-    try:
-        c_caption = data[1]
-    except:
-        pass
-    thumb = data[0]
-
-    duration = 0
-    metadata = extractMetadata(createParser(file_path))
-    if metadata and metadata.has("duration"):
-        duration = metadata.get('duration').seconds
-    if c_caption:
-        vid_list = ["filename", "filesize", "duration"]
-        new_tex = escape_invalid_curly_brackets(c_caption, vid_list)
-        caption = new_tex.format(filename=new_filename, filesize=humanbytes(
-            file.file_size), duration=timedelta(seconds=duration))
-    else:
-        caption = f"**{new_filename}**"
-    if thumb:
-        ph_path = await bot.download_media(thumb)
-        Image.open(ph_path).convert("RGB").save(ph_path)
-        img = Image.open(ph_path)
-        img.resize((320, 320))
-        img.save(ph_path, "JPEG")
-        c_time = time.time()
-
-    else:
-        try:
-            ph_path_ = await take_screen_shot(file_path, os.path.dirname(os.path.abspath(file_path)), random.randint(0, max(0, duration - 1)))
-            width, height, ph_path = await fix_thumb(ph_path_)
-        except Exception as e:
-            ph_path = None
-
-    value = 2090000000
-    if value < file.file_size:
-        await ms.edit("🚀 Try To Upload...  ⚡")
-        try:
-            client_to_use = app if app else bot
-            filw = await client_to_use.send_video(int(LOG_CHANNEL), video=metadata_path if _bool_metadata else file_path, thumb=ph_path, duration=duration, caption=caption, progress=progress_for_pyrogram, progress_args=("🚀 Try To Uploading...  ⚡",  ms, c_time))
-            from_chat = filw.chat.id
-            mg_id = filw.id
-            time.sleep(2)
-            await bot.copy_message(update.from_user.id, from_chat, mg_id)
-            await ms.delete()
-            
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            if ph_path and os.path.exists(ph_path):
-                os.remove(ph_path)
-                
-        except Exception as e:
-            neg_used = used - int(file.file_size)
-            used_limit(update.from_user.id, neg_used)
-            await ms.edit(str(e))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            if ph_path and os.path.exists(ph_path):
-                os.remove(ph_path)
-            return
-    else:
-        await ms.edit("🚀 Try To Upload...  ⚡")
-        c_time = time.time()
-        try:
-            await bot.send_video(update.from_user.id, video=metadata_path if _bool_metadata else file_path, thumb=ph_path, duration=duration, caption=caption, progress=progress_for_pyrogram, progress_args=("🚀 Try To Uploading...  ⚡",  ms, c_time))
-            await ms.delete()
-            
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            
-        except Exception as e:
-            neg_used = used - int(file.file_size)
-            used_limit(update.from_user.id, neg_used)
-            await ms.edit(str(e))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            return
-
-
-@Client.on_callback_query(filters.regex("aud"))
-async def aud(bot, update):
-
-    if not os.path.isdir("Metadata"):
-        os.mkdir("Metadata")
-
-    new_name = update.message.text
-    used_ = find_one(update.from_user.id)
-    used = used_["used_limit"]
-    name = new_name.split(":-")
-    new_filename = name[1]
-    file_path = f"downloads/{new_filename}"
-    message = update.message.reply_to_message
-    file = message.document or message.video or message.audio
-    hinata = message
-    total_used = used + int(file.file_size)
-    used_limit(update.from_user.id, total_used)
-    ms = await update.message.edit("🚀 Try To Download...  ⚡")
-    c_time = time.time()
-    try:
-        path = await bot.download_media(message=file, progress=progress_for_pyrogram, progress_args=("🚀 Try To Downloading...  ⚡",  ms, c_time))
-    except Exception as e:
-        neg_used = used - int(file.file_size)
-        used_limit(update.from_user.id, neg_used)
-        await ms.edit(str(e))
-        return
-    
-    _bool_metadata = find(int(message.chat.id))[2] 
-    
-    if _bool_metadata:
-        metadata = find(int(message.chat.id))[3]
-        metadata_path = f"Metadata/{new_filename}"
-        await add_metadata(path, metadata_path, metadata, ms)
-    else:
-        await ms.edit("🚀 Mode Changing...  ⚡")
-        
-    splitpath = path.split("/downloads/")
-    dow_file_name = splitpath[1]
-    old_file_name = f"downloads/{dow_file_name}"
-    os.rename(old_file_name, file_path)
-    duration = 0
-    metadata = extractMetadata(createParser(file_path))
-    if metadata and metadata.has("duration"):
-        duration = metadata.get('duration').seconds
-    user_id = int(update.message.chat.id)
-    data = find(user_id)
-    c_caption = data[1]
-    thumb = data[0]
-    if c_caption:
-        aud_list = ["filename", "filesize", "duration"]
-        new_tex = escape_invalid_curly_brackets(c_caption, aud_list)
-        caption = new_tex.format(filename=new_filename, filesize=humanbytes(
-            file.file_size), duration=timedelta(seconds=duration))
-    else:
-        caption = f"**{new_filename}**"
-
-    if thumb:
-        ph_path = await bot.download_media(thumb)
-        Image.open(ph_path).convert("RGB").save(ph_path)
-        img = Image.open(ph_path)
-        img.resize((320, 320))
-        img.save(ph_path, "JPEG")
-        await ms.edit("🚀 Try To Upload...  ⚡")
-        c_time = time.time()
-        try:
-            await bot.send_audio(update.message.chat.id, audio=metadata_path if _bool_metadata else file_path, caption=caption, thumb=ph_path, duration=duration, progress=progress_for_pyrogram, progress_args=("🚀 Try To Uploading...  ⚡",  ms, c_time))
-            await ms.delete()
-            
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            if os.path.exists(ph_path):
-                os.remove(ph_path)
-            
-        except Exception as e:
-            neg_used = used - int(file.file_size)
-            used_limit(update.from_user.id, neg_used)
-            await ms.edit(str(e))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            if os.path.exists(ph_path):
-                os.remove(ph_path)
-    else:
-        await ms.edit("🚀 Try To Upload...  ⚡")
-        c_time = time.time()
-        try:
-            await bot.send_audio(update.message.chat.id, audio=metadata_path if _bool_metadata else file_path, caption=caption, duration=duration, progress=progress_for_pyrogram, progress_args=("🚀 Try To Uploading...  ⚡",  ms, c_time))
-            await ms.delete()
-            
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            
-        except Exception as e:
-            await ms.edit(str(e))
-            neg_used = used - int(file.file_size)
-            used_limit(update.from_user.id, neg_used)
-            if os.path.exists(file_path):
-                os.remove(file_path)
+        keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ᴄʟɪᴄᴋ ꜰᴏʀ ᴍᴏʀᴇ •", callback_data="more")],
+            [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
+             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url=UPDATE_CHANNEL)],
+            [InlineKeyboardButton("ᴅᴏɴᴀᴛᴇ", callback_data='donate')]
+        ])
+        await query.message.reply_text(
+            text=START_TXT.format(mention=query.from_user.mention), 
+            reply_markup=keyboard
+        )
