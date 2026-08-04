c176261d5da9f31d7c6c9e36036c5255f76d728f
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-04 15:52:50 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -4,21 +4,14 @@ from pyrogram import filters, Client
 CMD = ["/"]
 
 
-
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
     start_t = time.time()
-    rm = await message.reply_text("Pinging....")
+    rm = await message.reply_text("<b><blockquote>ᴘɪɴɢɪɴɢ....</blockquote></b>")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Ping 🔥!\n\n{time_taken_s:.3f} ms")
+    await rm.edit(
+        f"<b><blockquote>ᴘɪɴɢ 🔥!\n\n"
+        f"⚡️ {time_taken_s:.3f} ᴍꜱ</blockquote></b>"
+    )
     return time_taken_s
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Back-Up Channel @JishuBotz
-# Developer @JishuDeveloper & @MadflixOfficials
