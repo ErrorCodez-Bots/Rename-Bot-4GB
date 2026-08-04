8561e82e498edd437ab58f998b439bb49d195879
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-04 15:52:03 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,23 +1,16 @@
-import os, sys, asyncio
+import os
+import sys
+import asyncio
 from config import *
 from pyrogram import filters, Client
 
 
-
-
 @Client.on_message(filters.command("restart") & filters.user(ADMIN))
 async def stop_button(bot, message):
-    msg = await bot.send_message(text="🔄 Processes Stoped. Bot Is Restarting...", chat_id=message.chat.id)       
+    msg = await bot.send_message(
+        text="<b><blockquote>🔄 ᴘʀᴏᴄᴇꜱꜱᴇꜱ ꜱᴛᴏᴘᴘᴇᴅ. ʙᴏᴛ ɪꜱ ʀᴇꜱᴛᴀʀᴛɪɴɢ...</blockquote></b>", 
+        chat_id=message.chat.id
+    )       
     await asyncio.sleep(3)
-    await msg.edit("✅️ Bot Is Restarted. Now You Can Use Me")
+    await msg.edit("<b><blockquote>✅️ ʙᴏᴛ ɪꜱ ʀᴇꜱᴛᴀʀᴛᴇᴅ. ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ.</blockquote></b>")
     os.execl(sys.executable, sys.executable, *sys.argv)
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Back-Up Channel @JishuBotz
-# Developer @JishuDeveloper & @MadflixOfficials
