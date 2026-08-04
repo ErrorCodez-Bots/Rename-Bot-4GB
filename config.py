3da23fdf830bf15a6a77d8b146a8b7871d726cdf
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-08-04 16:59:26 +0530

    Rename DATABASE_URL and DATABASE_NAME variables

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DB_URL = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 # Single Start Picture (Fallback)
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
